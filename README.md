7b138ee02c79a74a9a2bbb8e107e88c5b70b7307
songmilee <lsm941010@gmail.com>
2020-05-26 19:27:57 +0900

    add template

---
+++ README.md
@@ -0,0 +1,8 @@
+### Portfolio
+
+### 구현 기술
+- Server : Python Django
+- Front : React
+
+#### 필요 기능
+- 포트폴리오 보기
--- django_ppd/src/model/person.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Person(models.Model):
-    name = models.CharField(max_length=100, blank=False, null=False)
-    address = models.CharField(max_length=100, blank=True, null=False)
-    year = models.DecimalField(max_digits=9999)
-    month = models.DecimalField(max_digits=12)
-    day = models.DecimalField(max_digits=31)
-    #sex = models.BooleanField()
